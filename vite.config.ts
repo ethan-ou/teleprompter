4ae8023ad24adbec229699fa4df9d41f1efa23b1
ethan-ou <45083970+ethan-ou@users.noreply.github.com>
2024-07-06 12:30:10 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import { resolve } from "path";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [preact(), viteSingleFile()],
+  plugins: [preact()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
