698e172a9cee9b7850154fe6f2ef65e5482f253b
ethan-ou <45083970+ethan-ou@users.noreply.github.com>
2024-07-11 23:05:20 +1000

    Update index.tsx

--- src/features/content/index.tsx
+++ src/features/content/index.tsx
@@ -3,6 +3,7 @@ import { escape } from "@/lib/html-escaper";
 import { useNavbarStore } from "../navbar/store";
 import { useContentStore } from "./store";
 import { useShallow } from "zustand/react/shallow";
+import { useHotkeys } from "react-hotkeys-hook";
 
 export function Content() {
   const { fontSize, margin, status, opacity, horizontallyFlipped, verticallyFlipped, align } =
@@ -51,8 +52,24 @@ export function Content() {
     }
   });
 
+  // Overwrite ctrl+a to allow text selection without also selecting inputs.
+  const mainRef = useRef<HTMLElement | null>(null);
+  useHotkeys(
+    "ctrl+a",
+    () => {
+      const selection = window.getSelection();
+      if (mainRef.current && selection) {
+        const range = document.createRange();
+        range.selectNodeContents(mainRef.current);
+        selection.removeAllRanges();
+        selection.addRange(range);
+      }
+    },
+    { preventDefault: true },
+  );
+
   return (
-    <main>
+    <main ref={mainRef}>
       {status === "editing" ? (
         <div className="grid">
           {/* Use an invisible div to force an increase in textarea sizing.
