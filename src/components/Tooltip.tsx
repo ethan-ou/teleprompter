7dc18f31a2eb0e82250f6d9cb34c85b45f5ab5b8
ethan-ou <45083970+ethan-ou@users.noreply.github.com>
2024-07-14 13:19:46 +1000

    Update Tooltip.tsx

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -12,7 +12,7 @@ export function Tooltip({
   return (
     <span
       className={clsx(
-        "pointer-events-none absolute z-10 touch-none text-nowrap rounded bg-neutral-800 py-1.5 px-2 text-[0.8rem] text-white opacity-0 transition-all delay-300 group-hover/tooltip:opacity-100 group-aria-disabled/tooltip:hidden",
+        "pointer-events-none absolute z-10 touch-none text-nowrap rounded bg-neutral-800 py-1.5 px-2 text-[0.8rem] text-white opacity-0 transition-all delay-100 group-hover/tooltip:opacity-100 group-aria-disabled/tooltip:hidden",
         position,
         className,
       )}
