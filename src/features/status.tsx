29032e9239542805a801b2bd08359a9812251d1e
ethan-ou <45083970+ethan-ou@users.noreply.github.com>
2024-07-18 00:18:41 +1000

    Update status.tsx

--- src/features/status.tsx
+++ src/features/status.tsx
@@ -1,17 +1,19 @@
 import { clsx } from "@/lib/css";
 import { useNavbarStore } from "./navbar/store";
+import { useFullScreen } from "@/app/hooks";
 
 export const Status = ({ className, ...props }: React.ComponentPropsWithoutRef<"div">) => {
   const status = useNavbarStore((state) => state.status);
+  const fullscreen = useFullScreen();
 
   return (
     <div
       className={clsx(
         "pointer-events-none fixed z-30 size-full touch-none",
-        status === "started" && document.fullscreenElement
+        status === "started" && fullscreen.active
           ? "[box-shadow:inset_0px_0px_0px_5px_rgba(255,0,0,0.3)]"
           : "",
-        status === "editing" && document.fullscreenElement
+        status === "editing" && fullscreen.active
           ? "[box-shadow:inset_0px_0px_0px_5px_rgba(255,255,0,0.3)]"
           : "",
         className,
