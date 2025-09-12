c3d30b32d9f00438eaa765e8d4ebee2d6a55160e
Ethan Ou <45083970+ethan-ou@users.noreply.github.com>
2025-09-12 10:26:24 +1000

    Add extra servers

--- src/features/collaborate/store.ts
+++ src/features/collaborate/store.ts
@@ -3,7 +3,7 @@ import * as Y from "yjs";
 import { TrysteroProvider } from "@/app/y-webrtc-trystero";
 import { selfId } from "@/app/y-webrtc-trystero";
 import { Position } from "../content/store";
-import { joinRoom } from "trystero";
+import { joinRoom } from "trystero/mqtt";
 import { generatePassphrase } from "@/lib/generate-passphrase";
 
 const APP_ID = "voice-teleprompter-4DRPRcq3FJmdfwgHnKMOy";
@@ -60,7 +60,39 @@ export const useCollaborateStore = create<RoomState & RoomActions>()((set, get) 
     try {
       const ydoc = new Y.Doc();
       const provider = new TrysteroProvider(roomId, ydoc, {
-        trysteroRoom: joinRoom({ appId: APP_ID }, roomId),
+        trysteroRoom: joinRoom(
+          {
+            appId: APP_ID,
+            rtcConfig: {
+              iceServers: [
+                {
+                  urls: "stun:stun.relay.metered.ca:80",
+                },
+                {
+                  urls: "turn:standard.relay.metered.ca:80",
+                  username: "2067430dcd7003f6f22de535",
+                  credential: "NOp0t02fAg2VlzyY",
+                },
+                {
+                  urls: "turn:standard.relay.metered.ca:80?transport=tcp",
+                  username: "2067430dcd7003f6f22de535",
+                  credential: "NOp0t02fAg2VlzyY",
+                },
+                {
+                  urls: "turn:standard.relay.metered.ca:443",
+                  username: "2067430dcd7003f6f22de535",
+                  credential: "NOp0t02fAg2VlzyY",
+                },
+                {
+                  urls: "turns:standard.relay.metered.ca:443?transport=tcp",
+                  username: "2067430dcd7003f6f22de535",
+                  credential: "NOp0t02fAg2VlzyY",
+                },
+              ],
+            },
+          },
+          roomId,
+        ),
         maxConns: 5,
         filterBcConns: false,
       });
